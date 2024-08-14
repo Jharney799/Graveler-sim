aff2bf29746be003df9f9fc627df085d9974dd1c
Jharney799 <jharney799@gmail.com>
2024-08-14 16:30:18 -0700

    Update to code using multiprocessing

--- graveler.py
+++ graveler.py
@@ -1,25 +1,36 @@
 import random
+from multiprocessing import Pool, cpu_count
 from timeit import default_timer as timer
 from collections import Counter
 
 items = [1, 2, 3, 4]
-rolls = 0
-maxOnes = 0
 
-start = timer()
+def simulate_rolls(rolls_per_worker):
+    maxOnes = 0
+    for _ in range(rolls_per_worker):
+        numbers = Counter(random.choices(items, k=231))
+        ones_count = numbers[1]
+        if ones_count > maxOnes:
+            maxOnes = ones_count
+        
+        if ones_count >= 177:
+            break
+    return maxOnes
 
-while rolls < 1000000:
-    numbers = Counter(random.choices(items, k=231))
-    rolls += 1
+if __name__ == "__main__":
+    start = timer()
 
-    ones_count = numbers[1]
-    if ones_count > maxOnes:
-        maxOnes = ones_count
+    total_rolls = int(1e9)
+    num_workers = cpu_count()
+    print(num_workers)
+    rolls_per_worker = total_rolls // num_workers
 
-    if ones_count >= 177:
-        break
+    with Pool(num_workers) as pool:
+        results = pool.map(simulate_rolls, [rolls_per_worker] * num_workers)
+        print(results)
+    
+    max_ones = max(results)
+    print("Highest Ones Roll:", max_ones)
 
-end = timer()
-print("Time Elasped:", end - start)
-print("Highest Ones Roll:", maxOnes)
-print("Number of Roll Sessions: ", rolls)
+    end = timer()
+    print("Time Elapsed:", end - start)
